80a682be72b4be46f31a68efc029fc11649cd6af
greensgit <rajgreensomr@gmail.com>
2022-10-14 13:46:17 +0530

    new code

---
+++ src/org/cts/test/DemoClass.java
@@ -0,0 +1,21 @@
+package org.cts.test;
+
+public class DemoClass {
+	
+	
+	//ctrl+space and enter
+	private void studentName() {
+System.out.println("My name is Raj");
+	}
+	
+	public static void main(String[] args) {
+		
+		//classname objectname = new classname();
+		
+		DemoClass d = new DemoClass();  //object created - memory allocation
+		
+		//objectname.methodname
+		
+		d.studentName();
+	}
+}
